83e51873ecd274234f52337ed562ee46b997edef
AndBird <31888588+AndBird@users.noreply.github.com>
2021-04-13 14:01:01 +0800

    改低编译要求，系统VideoView可播放爱奇艺视频

--- renderer/src/main/java/com/android/cast/dlna/demo/renderer/MainActivity.java
+++ renderer/src/main/java/com/android/cast/dlna/demo/renderer/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.cast.dlna.core.Utils;
 import com.android.cast.dlna.dmr.DLNARendererService;
-import com.permissionx.guolindev.PermissionX;
+//import com.permissionx.guolindev.PermissionX;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PermissionX.init(this)
+        /*PermissionX.init(this)
                 .permissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
-                .request((allGranted, grantedList, deniedList) -> resetWifiInfo());
+                .request((allGranted, grantedList, deniedList) -> resetWifiInfo());*/
         DLNARendererService.startService(this);
     }
 
